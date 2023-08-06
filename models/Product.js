8c9c2295ef9d2acb61b837f6ab0beab1876d4eba
athenamw <wallisathena@gmail.com>
2023-08-05 22:01:14 -0700

    created product.js columns

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,5 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, DECIMAL } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -9,7 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: INT,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: INT,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isInt: true,
+      },
+    },
+    category_id: {
+      type: INT,
+      references: {
+        model: 'category',
+        key: 'id',
+        unique: false,
+      },
+    },
   },
   {
     sequelize,
